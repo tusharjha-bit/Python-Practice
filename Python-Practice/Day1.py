828b044a146fc8e5b40d78f768fc977af71ca891
tushar jha <tusharjha944@gmai.com>
2026-08-04 19:51:06 +0530

    Day 1 Python Practice

---
+++ Python-Practice/Day1.py
@@ -0,0 +1,64 @@
+# This is first python file
+import types
+
+
+print("hello world")
+
+# this is python data types
+a = 2
+b = 4
+print( a + b )
+print(type(a))
+print(type(b))
+
+# this is python string
+Name = "tushar"
+City = "delhi"
+print(Name)
+print(City)
+print(type(Name))
+print(type(City))
+
+# this is float data type
+c = 3.2
+d = 4.3
+print(c + d)
+print(type(c))
+print(type(d))
+
+# this is tuple
+colour = ("red", "green", "blue")
+print(colour)
+print(type(colour))
+
+# this is list
+fruites = ["apples", "mangoes", "banana"]
+print(fruites)
+print(type(fruites))
+
+# this is set 
+nums = {1,2,3,4,5}
+print(nums)
+print(type(nums))
+
+# boolen 
+Tushar = True
+Tussu = False 
+print(Tushar)
+print(Tussu)
+print(type(Tushar))
+print(type(Tussu))
+
+# dictionary data type
+student = {"name": "tushar",
+           "age": "20",
+           "class": "12th"
+           }
+print(student)
+print(type(student))
+
+# none 
+x = None
+
+print(x)
+print(type(x))
