aa119fa559498a0c2467b5ea178cb520b2b0ad67
tushar jha <tusharjha944@gmai.com>
2026-08-06 13:58:30 +0530

    Day3 practice

---
+++ Python-Practice/Day3.py
@@ -0,0 +1,65 @@
+# HI THIS IS TUSHAR HERE DAY THREE.
+ 
+name = "tushar"
+print(str(name))
+
+name = "tushar"
+print(repr(name))
+
+t = "hello\npyhton"
+print(str(t))
+print(repr(t))
+
+# question to practice
+
+x = "python"
+
+print(str(x))
+print(repr(x))
+
+print(str(25))
+print(repr(25))
+
+# number operations
+a = 15
+b = 4
+print( a + b )
+print( a - b )
+print( a * b )
+print( a / b )
+print( a // b )
+print( a % b )
+print( a ** b )
+
+#  practice
+
+print(20 // 6 )
+print(20 % 6 )
+print( 3 ** 6 )
+
+# and and or opperations 
+print(10 > 5 and 8 > 2) 
+print(5 > 2 or 10 < 8) 
+print(10 > 5 and 8 < 2)
+print(10 > 5 or 8 < 2)
+
+# maths liabrary
+import math
+print(math.floor(3.3))
+print(math.floor(-3.9))
+
+print(math.trunc(4.5))
+print(math.trunc(-4.5))
+
+# practice questions
+print(math.floor(9.3))
+print(math.floor(-9.3))
+print(math.trunc(9.3))
+print(math.trunc(-9.3))
+
+
+nums = [1,2,3,4]
+
+random.shuffle(nums)
+
+print(nums)
