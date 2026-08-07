e27bc2fd13904ab5c0930b9bcdabdc8a6ba7a27e
tushar jha <tusharjha944@gmai.com>
2026-08-07 17:05:45 +0530

    day4 Practice

---
+++ Day4.py
@@ -0,0 +1,99 @@
+#  level 1 questions 
+# 1 question 
+a = 25
+print(type(a))
+
+# 2 question
+a = 15
+b = 4
+print( a + b )
+print( a - b )
+print( a * b )
+print( a / b )
+
+#  3 question 
+print( 20 // 3 )
+print( 20 % 3 )
+
+# 4 questions 
+nums = 12
+square = nums ** 2
+print(square)
+
+# 5 question 
+nums = 4
+cube = nums ** 3
+print(cube)
+
+# level 2 
+
+# 6 questions 
+
+num = "450"
+interger = int(num)
+print(type(interger))
+print(num)
+
+# 7 questions 
+
+num = 45
+nums = str(num)
+
+print(type("num"))
+print(num)
+
+# 8 questions
+'''
+num = float(input("12.3: "))
+num_int = int(num)
+
+print("12:", num_int)
+'''
+
+# level 3 questions
+# 9 question 
+
+import math
+
+R = float(input("12:",))
+area = 3.14 * R * R
+
+print("area of circle:", area)
+
+# 10 question
+
+lenght = 12
+breath = 34
+area = lenght * breath 
+
+print("area of rectange:", area)
+
+# 11 questions
+
+no = float(input("12:",))
+no2 = float(input("14:",))
+
+if a > b:
+ print("Maximum:", a)
+else :
+  print("maximum:",b)
+
+#   12 questions
+
+no1 = float(input("14:",))
+no2 = float(input("15:",))
+
+if no1 < no2 :
+   print("minimum:", no1)
+
+else :
+   print("minimum:", no2)
+   
+
+
+
+
+
+
+
+            
